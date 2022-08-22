a13d8db45eb65d40bea4621e5778882e74fd6df3
Emmanuel <mailkumah@gmail.com>
2022-08-22 12:42:37 +0000

    Add readme file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+      fontFamily: {
+        body: ["Outfit"],
+      },
+    },
+  },
+  plugins: [],
+};
